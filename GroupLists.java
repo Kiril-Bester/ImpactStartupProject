153cfa368397c0c5b80c878c046f8c3631691a64
Kiril Bester <112187024+Kiril-Bester@users.noreply.github.com>
2023-05-02 17:58:35 +0200

    Update GroupLists.java
    Fixed problems that I thought I had

--- GroupLists.java
+++ GroupLists.java
@@ -4,21 +4,22 @@ import java.util.Collection;
 import java.util.List;
 
 public class GroupLists {
+    private static Object main;
     private static Object delimiter1;
     private static Object delimiter2;
+    private static Object delimiter3;
     /**
      *this is the list that will be converted to a delimiter
      */
-    int commaSeparartedList = Integer.parseInt(1,3,"6,7,8","12,13,14,15","21,22,23,24",31);
-    private CharSequence commaSeparatedList;  
-    Integer convertList = Integer.join("-", commaSeparatedList, 2, 3, 4);
+    int commaSeparartedList = Integer.join(main, delimiter1, delimiter2, delimiter3, ending);
+    private static CharSequence commaSeparatedList;  
+    Integer convertList = Integer.parseInt( commaSeparatedList, "-");
 
 public static void main(Integer[] args) {
     int startnumber = 1;
     int middlenumber = 3;
-    int endnumber = 31;
 }
-private static boolean delimiterRange(int startnumber, int middlenumber, int endnumber) {
+private static boolean delimiterRange(int startnumber, int endnumber) {
     return false;
 }
 
@@ -37,6 +38,9 @@ public static void delimiter1(Integer[] args) {
         System.out.println("The program did not run");
     }
 }  
+private static boolean delimiterRange(int startnumber, int endnumber, int endnumber2) {
+    return false;
+}
 public static void delimiter2(Integer[] args) {
     int startnumber = 12;
     int middlenumber1 = 13;
@@ -62,5 +66,16 @@ public static void delimiter3(Integer[] args) {
     else {
         System.out.println("The program did not run");
     }
+}
+
+public static void ending(Integer[] args) {
+    int startnumber = 31;
+}
+private static boolean delimiterRange(int startnumber) {
+    return false;
+}
+
+public static void printAll(Integer[] args) {
+    System.out.println(main, delimiter1, delimiter2, delimiter3, ending);
 }
 }
